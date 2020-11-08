8ec736d80bbba6a131a8023ab08b9c1b1703e5eb
Alex Vetsavong <avetsa2@illinois.edu>
2020-11-08 17:35:48 -0600

    add WIP files for control rom and data types. Empty datapath file.

---
+++ skeleton_code/hdl/datapath.sv
@@ -0,0 +1,23 @@
+import rv32i_types::*;
+
+module datapath();
+// TODO: create control_rom module
+
+// TODO: instantiate modules for each stage
+
+// IF
+
+
+// ID
+
+
+// EX
+
+
+// MEM
+
+
+// WB
+
+
+endmodule
