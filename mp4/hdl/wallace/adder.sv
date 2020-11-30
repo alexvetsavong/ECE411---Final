c76493d7d2a1ec1538f7f2526c1b3dde77b51637
Mohan Li <mohanl2@linux-a2.ews.illinois.edu>
2020-11-30 09:30:39 -0600

    MUL: half adder & full adder

---
+++ mp4/hdl/wallace/adder.sv
@@ -0,0 +1,26 @@
+// Adder.sv - full adder and half adder
+// ^ == XOR
+module halfadder(
+  input logic a,
+  input logic b,
+  output logic s,
+  output logic c
+);
+assign s = a ^ b;
+assign c = a & b;
+
+endmodule: halfadder
+
+module fulladder(
+  input logic a,
+  input logic b,
+  input logic cin,
+  output logic s,
+  output logic c
+);
+assign s = a ^ b ^ cin;
+assign c = (a & b) | (a & cin) | (a & cin);
+
+endmodule: halfadder
+
+// TODO: 32-bit Carry Propagate Adder or CLA for the last stage
