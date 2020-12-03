4602f40419c3c85e32ea6641ff11acb963f726b0
Alex Vetsavong <avetsa2@illinois.edu>
2020-12-02 23:55:51 -0600

    remove some accidental merges from peter's branch.

--- mp4/hdl/processor/control_rom.sv
+++ mp4/hdl/processor/control_rom.sv
@@ -41,7 +41,6 @@ function void set_defaults();
     ctrl.opcode = opcode;
     ctrl.funct3 = funct3;
 
-    ctrl.commit = 1'b0;
 endfunction
 
 always_comb begin
@@ -52,7 +51,6 @@ always_comb begin
             ctrl.immmux_sel = immmux::u_imm;
             ctrl.regfilemux_sel = regfilemux::u_imm; 
             ctrl.load_regfile = 1'b1;
-            ctrl.commit = 1'b1;
         end
         op_auipc: 
         begin
@@ -62,7 +60,6 @@ always_comb begin
             ctrl.alumux2_sel = alumux::imm;
             ctrl.regfilemux_sel = regfilemux::alu_out;
             ctrl.load_regfile = 1'b1;
-            ctrl.commit = 1'b1;
         end
         
         op_jal: 
@@ -74,7 +71,6 @@ always_comb begin
             ctrl.alumux2_sel = alumux::imm;
             ctrl.regfilemux_sel = regfilemux::pc_plus4;
             ctrl.load_regfile = 1'b1;
-            ctrl.commit = 1'b1;
         end
         
         op_jalr: 
@@ -86,7 +82,6 @@ always_comb begin
             ctrl.alumux2_sel = alumux::imm;
             ctrl.regfilemux_sel = regfilemux::pc_plus4;
             ctrl.load_regfile = 1'b1;
-            ctrl.commit = 1'b1;
         end
         op_br: 
         begin
@@ -97,7 +92,6 @@ always_comb begin
             ctrl.aluop = alu_add;
             ctrl.alumux1_sel = alumux::pc_out;
             ctrl.alumux2_sel = alumux::imm;
-            ctrl.commit = 1'b1;
         end
         op_load: 
         begin
@@ -119,7 +113,6 @@ always_comb begin
                     default: begin end
                 endcase
             ctrl.load_regfile = 1'b1;
-            ctrl.commit = 1'b1;
         end
         op_store:
         begin 
@@ -133,7 +126,6 @@ always_comb begin
             ctrl.mem_write = 1'b1;
 
             ctrl.load_regfile = 1'b0;
-            ctrl.commit = 1'b1;
         end
         op_imm: 
         begin
@@ -141,7 +133,6 @@ always_comb begin
             ctrl.alumux1_sel = alumux::rs1_out;
             ctrl.alumux2_sel = alumux::imm;
             ctrl.load_regfile = 1'b1;
-            ctrl.commit = 1'b1;
             case (arith_funct3_t'(funct3))
                 slt: 
                 begin 
@@ -173,7 +164,7 @@ always_comb begin
                 default: 
                 begin 
                     ctrl.regfilemux_sel = regfilemux::alu_out;
-                    1'b0: ctrl.aluop = alu_ops'(funct3);
+                    ctrl.aluop = alu_ops'(funct3);
                 end
             endcase
         end
@@ -183,7 +174,6 @@ always_comb begin
             ctrl.alumux1_sel = alumux::rs1_out;
             ctrl.alumux2_sel = alumux::rs2_out;
             ctrl.load_regfile = 1'b1;
-            ctrl.commit = 1'b1;
             case (arith_funct3_t'(funct3))
                 slt: 
                 begin 
